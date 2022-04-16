c4e92138c32b2025f84351730809f5d6dafcd453
Charlie Puente <puentematos@gmail.com>
2022-04-16 00:31:12 -0400

    Added header, container

--- client/src/pages/landing/index.tsx
+++ client/src/pages/landing/index.tsx
@@ -1,1 +1,13 @@
 import React from "react";
+
+function LandingPage() {
+    return (
+        <main className="h-screen w-screen">
+            <section className="container mx-auto mt-16">
+            <h1 className="text-white underline decoration-violet-600">Welcome!</h1>
+            </section>
+        </main>
+    )
+} 
+
+export default LandingPage;
