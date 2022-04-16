2ce1499b49c51d11098e96df4e2e6c66a4fbe8cf
Puentebravo <puentematos@gmail.com>
2022-04-16 13:04:19 -0400

    Added button

--- client/src/pages/landing/index.tsx
+++ client/src/pages/landing/index.tsx
@@ -1,10 +1,12 @@
 import React from "react";
+import "./landing.css"
 
 function LandingPage() {
     return (
         <main className="h-screen w-screen">
-            <section className="container mx-auto mt-16">
-            <h1 className="text-white underline decoration-violet-600">Welcome!</h1>
+            <section className="container mx-auto my-64">
+            <h1 className="text-white underline text-7xl decoration-violet-600">Welcome!</h1>
+            <button className="rounded-full bg-violet-600 text-white mt-10 px-8 py-1 shadow-lg shadow-violet-600/50 font-bold transition duration-300 ease-in-out hover:shadow-violet-600/100">Come on in!</button>
             </section>
         </main>
     )
