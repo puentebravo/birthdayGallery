9ac1bb2cc11d24de9c2adbfa4e8b4b7bf7ef949f
Charlie Puente <puentematos@gmail.com>
2022-04-20 12:20:19 -0400

    Added react routing to app

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
 import './App.css';
+import Home from './pages/home';
 import LandingPage from './pages/landing';
 
 function App() {
   return (
-    <div className="App">
-     <LandingPage/>
-    </div>
+    <Router basename={`${process.env.PUBLIC_URL}/`}>
+      <Routes>
+        <Route path="/" element={<LandingPage/>} />
+        <Route path="/gallery" element={<Home/>} />
+      </Routes>
+    </Router>
   );
 }
 
