313fb3a768dadcad6f0ec9809a9045b6f39d0569
Charlie Puente <puentematos@gmail.com>
2022-04-20 12:21:17 -0400

    Added link, corrected styling in landing page

--- client/src/pages/landing/index.tsx
+++ client/src/pages/landing/index.tsx
@@ -1,12 +1,14 @@
 import React from "react";
-import "./landing.css"
+import { Link } from "react-router-dom";
+// import "./landing.css"
 
 function LandingPage() {
+
     return (
         <main className="h-screen w-screen">
-            <section className="container mx-auto my-64">
+            <section className="container mx-64 my-64">
             <h1 className="text-white underline text-7xl decoration-violet-600">Welcome!</h1>
-            <button className="rounded-full bg-violet-600 text-white mt-10 px-8 py-1 shadow-lg shadow-violet-600/50 font-bold transition duration-300 ease-in-out hover:shadow-violet-600/100">Come on in!</button>
+            <button className="rounded-full bg-violet-600 text-white mt-10 px-8 py-1 shadow-lg shadow-violet-600/50 font-bold transition duration-300 ease-in-out hover:shadow-violet-600/100"><Link to="/gallery">Come on in!</Link></button>
             </section>
         </main>
     )
