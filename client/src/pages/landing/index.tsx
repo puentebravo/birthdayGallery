dbfc0fea619d2ea29cb00f41ec69838918ed30fc
Charlie Puente <puentematos@gmail.com>
2022-04-22 16:34:01 -0400

    Changed font to Square peg/cursive styling

--- client/src/pages/landing/index.tsx
+++ client/src/pages/landing/index.tsx
@@ -7,8 +7,8 @@ function LandingPage() {
     return (
         <main className="h-screen w-screen">
             <section className="container mx-64 my-64">
-            <h1 className="text-white underline text-7xl decoration-violet-600">Welcome!</h1>
-            <button className="rounded-full bg-violet-600 text-white mt-10 px-8 py-1 shadow-lg shadow-violet-600/50 font-bold transition duration-300 ease-in-out hover:shadow-violet-600/100"><Link to="/gallery">Come on in!</Link></button>
+            <h1 className="text-white underline text-8xl font-cursive decoration-violet-600">Welcome!</h1>
+            <button className="rounded-full bg-violet-600 text-white mt-10 px-8 py-1 mx-7 shadow-lg shadow-violet-600/50 font-bold transition duration-300 ease-in-out hover:shadow-violet-600/100"><Link to="/gallery">Come on in!</Link></button>
             </section>
         </main>
     )
