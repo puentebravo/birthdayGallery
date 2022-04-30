b8f9d5636bb6df6cc5606e58f13f6e94d740d3ff
Puentebravo <puentematos@gmail.com>
2022-04-30 12:08:56 -0400

    Scaffolded out gallery container

---
+++ client/src/components/galleryContainer.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+
+function GalleryContainer() {
+    return (
+        <section className="grid grid-cols-3 gap-6">
+
+        </section>
+    )
+}
+
+export default GalleryContainer;
