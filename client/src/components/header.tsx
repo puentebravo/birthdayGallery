4c1c58560b90a7a85e1958bca2666709917005f8
Puentebravo <puentematos@gmail.com>
2022-04-30 11:41:55 -0400

    Fixed header positioning, width

--- client/src/components/header.tsx
+++ client/src/components/header.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 function Header() {
     return (
-        <header className="container bg-violet-600 text-white justify-center h-24">
+        <header className="container mx-auto min-w-full bg-violet-600 text-white justify-center h-24">
             <h1 className="text-center text-6xl pt-6 font-bold font-cursive">Art in the time of Covid</h1>
         </header>
     )
