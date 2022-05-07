32870cdccd34c58915bc62a8161da46dc0732131
Puentebravo <puentematos@gmail.com>
2022-05-07 11:52:41 -0400

    Imported imgdata interface, fixed styling on imgCard

--- client/src/components/imgCard.tsx
+++ client/src/components/imgCard.tsx
@@ -1,9 +1,13 @@
 import React from "react";
+import type { imgData } from "../utils/imageList";
 
-function ImageCard() {
+function ImageCard(props: imgData) {
+
+
     return (
-        <article className="container">
-            
+        <article className="container mx-auto mt-10 border-8 border-violet-600 rounded-lg w-auto">
+            <img className="border-solid border-2 rounded border-white w-72 h-72 mx-auto" src={props.src} alt={props.alt} />
+            <figcaption className="text-white text-center mt-2 ">{props.caption}</figcaption>
         </article>
     )
 }
