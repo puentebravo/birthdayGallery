1ca6867b69e56b9db95d78d5275d9b1ea25d3b1b
Puentebravo <puentematos@gmail.com>
2022-05-07 11:52:17 -0400

    Added styling, component map to gallery container

--- client/src/components/galleryContainer.tsx
+++ client/src/components/galleryContainer.tsx
@@ -1,11 +1,14 @@
 import React from "react";
-
+import { imgList } from "../utils/imageList"
+import ImageCard from "./imgCard";
 
 
 function GalleryContainer() {
     return (
-        <section className="grid grid-cols-3 gap-6">
-
+        <section className="grid grid-cols-3 gap-6 ">
+            {imgList.map( img => {
+               return <ImageCard src={img.src} alt={img.alt} caption={img.caption}/>
+            })}
         </section>
     )
 }
