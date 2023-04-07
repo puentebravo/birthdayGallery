1f5bb71d81df3845f14728e738e202af67918346
Puentebravo <puentematos@gmail.com>
2023-04-07 19:56:44 -0400

    Added text to header

--- client/src/components/header.tsx
+++ client/src/components/header.tsx
@@ -4,7 +4,8 @@ import React from "react";
 function Header() {
     return (
         <header className="container mx-auto min-w-full bg-violet-600 text-white justify-center h-24">
-            <h1 className="text-center text-6xl pt-6 font-bold font-cursive">Art in the time of Covid</h1>
+            <h1 className="text-center text-5xl pt-6 font-bold font-cursive">Art in the time of Covid</h1>
+            <p className="text-center font-cursive text-l lg:text-xl">By April Sandmeyer</p>
         </header>
     )
 }
