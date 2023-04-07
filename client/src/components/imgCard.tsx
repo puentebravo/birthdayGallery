76c75160c87460328458b7ff43c420563bae1c5d
Puentebravo <puentematos@gmail.com>
2023-04-07 19:57:17 -0400

    Thinned out border around image cards

--- client/src/components/imgCard.tsx
+++ client/src/components/imgCard.tsx
@@ -5,7 +5,7 @@ function ImageCard(props: imgData) {
 
 
     return (
-        <article className="container mx-auto mt-10 border-8 border-violet-600 rounded-lg w-auto h-auto">
+        <article className="container mx-auto mt-10 border-4 border-violet-600 rounded-lg w-auto h-auto">
             <img className="border-solid border-2 rounded border-white w-72 h-72 mx-auto" src={props.src} alt={props.alt} />
             <figcaption className="text-white text-center mt-2 ">{props.caption}</figcaption>
         </article>
