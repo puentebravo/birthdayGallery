9438de86b882d9fdf754ecdc848934a753db5251
Puentebravo <puentematos@gmail.com>
2023-04-07 23:46:06 -0400

    Added image imports

--- client/src/utils/imageList.tsx
+++ client/src/utils/imageList.tsx
@@ -1,4 +1,17 @@
-import testImage from "../assets/images/placeHolder300.png"
+import abstractColor from "../assets/images/abstractColors.jpg";
+import affirmations from "../assets/images/affirmations.jpg"
+import april from "../assets/images/april.jpg"
+import christmas from "../assets/images/christmas.jpg"
+import coffee from "../assets/images/coffee.jpg";
+import gratitude from "../assets/images/gratitude.jpg"
+import kFiorito from "../assets/images/kfiorito.jpg"
+import mayaSwing from "../assets/images/mayaSwing.jpg"
+import ruby1 from "../assets/images/ruby1.jpg"
+import ruby2 from "../assets/images/ruby2.jpg"
+import spiralCube from "../assets/images/spiralCube.jpg"
+import vanGogh from "../assets/images/vanGogh.jpg"
+import zoom from "../assets/images/zoom.jpg"
+import runningTime from "../assets/images/runningTime.jpg"
 
 interface imgData {
     src: string,
@@ -8,54 +21,79 @@ interface imgData {
 
 const imgList: imgData[] = [
     {
-        src: testImage,
-        caption: "A test image",
+        src: abstractColor,
+        caption: "Abstract Colors, flowing",
+        alt: "Abstract colors, flowing around the page"
+    },
+    {
+        src: affirmations,
+        caption: "Powerful affirmations",
+        alt: "Words of affirmation drawn on the outline of a hand"
+    },
+    {
+        src: april,
+        caption: "A Self Portrait",
+        alt: "A self portrait of the artist"
+    },
+    {
+        src: christmas,
+        caption: "Christmas Time",
+        alt: "An image of a childhood christmas tree"
+    },
+    {
+        src: coffee,
+        caption: "Coffee in charcoal",
+        alt: "coffee cups drawn using charcoal"
+    },
+    {
+        src: gratitude,
+        caption: "Words of Gratitude",
         alt: "A block image measuring 300x300 pixels"
     },
     {
-        src: testImage,
-        caption: "A test image",
-        alt: "A block image measuring 300x300 pixels"
+        src: kFiorito,
+        caption: "A drawing of Kathleen Fiorito",
+        alt: "colored pencil drawing of Kathleen Fiorito and her dog"
     },
     {
-        src: testImage,
-        caption: "A test image",
-        alt: "A block image measuring 300x300 pixels"
+        src: mayaSwing,
+        caption: "Maya Caballero on her iconic swing",
+        alt: "The folk rock artist Maya Caballero performing on her iconic floating swingset"
     },
     {
-        src: testImage,
-        caption: "A test image",
-        alt: "A block image measuring 300x300 pixels"
+        src: ruby1,
+        caption: "A tale of the past, Part 1",
+        alt: "A tale from April's past, part 1"
     },
     {
-        src: testImage,
-        caption: "A test image",
-        alt: "A block image measuring 300x300 pixels"
+        src: ruby2,
+        caption: "A tale of the past, Part 2",
+        alt: "A tale from april's past, part 2"
     },
     {
-        src: testImage,
-        caption: "A test image",
-        alt: "A block image measuring 300x300 pixels"
+        src: spiralCube,
+        caption: "Colors spiraling into eternity",
+        alt: "Colors spiraling behind a blue cube"
     },
     {
-        src: testImage,
-        caption: "A test image",
-        alt: "A block image measuring 300x300 pixels"
+        src: runningTime,
+        caption: "Time, Running through eternity",
+        alt: "A colorful image of a clock, running through the universe"
     },
     {
-        src: testImage,
-        caption: "A test image",
-        alt: "A block image measuring 300x300 pixels"
+        src: vanGogh,
+        caption: "A mysterious, Red haired figure",
+        alt: "A drawing of an unnamed, red-haired person"
     },
     {
-        src: testImage,
-        caption: "A test image",
-        alt: "A block image measuring 300x300 pixels"
+        src: zoom,
+        caption: "Near or far, we all stick together",
+        alt: "A drawing of a zoom video call"
     },
 ]
 
 export type {
-    imgData, 
+    imgData,
 }
 
-export {imgList}
+export { imgList }
