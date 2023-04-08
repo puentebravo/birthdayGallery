a1e3f5c8d1086b80bec2108effb5a1c0007a5d29
Puentebravo <puentematos@gmail.com>
2023-04-08 00:51:27 -0400

    Added modal, typing to imgcard

--- client/src/components/imgCard.tsx
+++ client/src/components/imgCard.tsx
@@ -1,14 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import type { imgData } from "../utils/imageList";
+import Modal from "./modal";
 
 function ImageCard(props: imgData) {
 
+    const [show, setShow] = useState<boolean>(false)
+
+    const handleOpen = () => {
+        setShow(true)
+        
+    }
+
 
     return (
-        <article className="container mx-auto mt-10 border-4 border-violet-600 rounded-lg w-auto h-auto">
-            <img className="border-solid border-2 rounded border-white w-72 h-72 mx-auto" src={props.src} alt={props.alt} />
-            <figcaption className="text-white text-center mt-2 ">{props.caption}</figcaption>
-        </article>
+        <>
+            <article className="container mx-auto mt-10 border-4 border-violet-600 rounded-lg w-auto h-auto">
+                <img className="border-solid border-2 rounded border-white w-72 h-72 mx-auto" src={props.src} alt={props.alt} />
+                <figcaption className="text-white text-center mt-2 ">{props.caption}</figcaption>
+                <button className="w-full bg-violet-600 text-white mt-2 shadow-lg shadow-violet-600/50" onClick={handleOpen}>Zoom In</button>
+            </article>
+            <Modal status={show} setStatus={setShow} imgAlt={props.alt} imgCaption={props.caption} imgSrc={props.src} />
+        </>
     )
 }
 
