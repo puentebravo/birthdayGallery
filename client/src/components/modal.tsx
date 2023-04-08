7d3275ba61200d55796086e9073d3a3ec7dbc0cf
Puentebravo <puentematos@gmail.com>
2023-04-08 00:50:27 -0400

    Added modal component

---
+++ client/src/components/modal.tsx
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+
+
+
+
+interface modalOpts {
+    status: boolean,
+    setStatus: (params: any) => any,
+    imgSrc: string,
+    imgAlt: string,
+    imgCaption: string
+}
+
+
+function Modal(props: modalOpts) {
+
+
+    const handleClose = () => {
+        props.setStatus(false)
+    }
+    
+
+    return (
+        <section className={`modal ${props.status ? 'show' : ""}`} onClick={handleClose}>
+            <div className="modal-content" onClick={e => e.stopPropagation()}>
+                <div className="modal-header">
+                    <h3 className="modal-title font-cursive">A Closer Look</h3>
+                </div>
+                <div className="modal-body">
+                    <img className="h-fit w-fit mx-auto"  src={props.imgSrc} alt={props.imgAlt}/>
+                    <figcaption className="text-white text-center mt-2">{props.imgCaption}</figcaption>
+                </div>
+                <div className="modal-footer">
+                    <button className="w-full bg-violet-600 text-white rounded-b-md" onClick={handleClose}>Close</button>
+                </div>
+
+            </div>
+        </section>
+    )
+}
+
+export default Modal;
