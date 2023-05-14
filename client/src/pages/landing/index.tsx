3872893ee50616605b86b4f23dcd1d8ddb157424
Puentebravo <puentematos@gmail.com>
2023-05-14 17:47:02 -0400

    Changed button text

--- client/src/pages/landing/index.tsx
+++ client/src/pages/landing/index.tsx
@@ -8,7 +8,7 @@ function LandingPage() {
         <main className="h-screen w-screen">
             <section className="container mx-auto text-center my-64">
             <h1 className="text-white underline text-8xl font-cursive decoration-violet-600">Welcome!</h1>
-            <button className="rounded-full bg-violet-600 text-white mt-10 px-8 py-1 mx-7 shadow-lg shadow-violet-600/50 font-bold transition duration-300 ease-in-out hover:shadow-violet-600/100"><Link to="/gallery">Come on in!</Link></button>
+            <button className="rounded-full bg-violet-600 text-white mt-10 px-8 py-1 mx-7 shadow-lg shadow-violet-600/50 font-bold transition duration-300 ease-in-out hover:shadow-violet-600/100"><Link to="/gallery">Gallery</Link></button>
             </section>
         </main>
     )
